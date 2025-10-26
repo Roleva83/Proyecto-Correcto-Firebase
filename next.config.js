b3770f65ccdc6018a9aed20b9b0c60cae207c66a
ROCIO LECHUGA VARGAS <rociotraffickerdigital@gmail.com>
2025-10-26 21:21:08 +0000

    fix: corregir apphosting.yaml y next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    allowedDevOrigins: [
-      '9082-firebase-studio-1751801531699.cluster-axf5tvtfjjfekvhwxwkkkzsk2y.cloudworkstations.dev'
-    ]
-  }
 }
 
 module.exports = nextConfig
