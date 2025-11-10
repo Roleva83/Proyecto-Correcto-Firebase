2f963796606ec051c65b9bb218d41eda468119f5
ROCIO LECHUGA VARGAS <rociotraffickerdigital@gmail.com>
2025-11-10 14:05:51 +0000

    fix: eliminar carpeta functions incompatible con App Hosting

--- functions/src/genkit/index.ts
+++
@@ -1,4 +0,0 @@
-export { lolaChatFlow } from './lola-chat-flow';
-export { analyzeReviewFlow } from './analyze-review-flow';
-export { analyzeMenuFlow } from './analyze-menu-flow';
-export { strategyRecommendationsFlow } from './strategy-recommendations-flow';
--- functions/src/index-old.ts
+++
@@ -1,8 +0,0 @@
-import * as admin from 'firebase-admin';
-
-// Inicializar Firebase Admin
-admin.initializeApp();
-
-// Exportar todas las functions
-export * from './lola-chat';
-export * from './analyze-reputation';
